28716e8bdccc24d124aba5ce943e55938690081a
Firman Firdaus <firmanfirdaus542@gmail.com>
2025-03-25 16:09:18 +0700

    update tampilan laporan keuangan

--- assets/js/lap_keuangan.js
+++ assets/js/lap_keuangan.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // tableIuran();
     $("#detail_keuangan").hide();
     $("#div-table-lap-keuangan").hide();
 
-    $("#preview").click(function() {
+    $("#preview").click(function () {
         let bulan = $("#bulan").val();
         let tahun = $("#tahun").val();
 
@@ -19,7 +19,7 @@ $(document).ready(function() {
         tableKeuangan(bulan, tahun);
     });
 
-    $("#export_excel_keuangan").click(function() {
+    $("#export_excel_keuangan").click(function () {
         let bulan = $("#bulan").val();
         let tahun = $("#tahun").val();
 
@@ -31,6 +31,22 @@ $(document).ready(function() {
         // Redirect ke fungsi export di controller dengan parameter bulan & tahun
         window.location.href = BASE_URL + "Laporan/export_excel_keuangan?bulan=" + bulan + "&tahun=" + tahun;
     });
+
+    let today = new Date();
+
+    // Pastikan timezone lokal digunakan
+    let todayStr = today.toLocaleDateString('sv-SE'); // Format YYYY-MM-DD
+
+    // Mendapatkan tanggal 30 hari yang lalu dengan memastikan waktu direset
+    let pastDate = new Date();
+    pastDate.setDate(today.getDate() - 30);
+    pastDate = new Date(pastDate.getFullYear(), pastDate.getMonth(), pastDate.getDate()); // Reset jam
+
+    let pastDateStr = pastDate.toLocaleDateString('sv-SE'); // Format YYYY-MM-DD
+
+    // Menetapkan nilai default pada input date
+    document.getElementById("date_end").value = todayStr;
+    document.getElementById("date_start").value = pastDateStr;
 });
 
 function tableKeuangan(bulan, tahun) {
