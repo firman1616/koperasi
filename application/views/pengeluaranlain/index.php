6007282032b109aa6a5ba7f4b8ffdf5bad71641d
firman1616 <firmanfirdaus542@gmail.com>
2025-04-28 07:17:30 +0700

    updatetanggal pengeluaran

--- application/views/pengeluaranlain/index.php
+++ application/views/pengeluaranlain/index.php
@@ -25,7 +25,7 @@
                     <form action="" id="pengeluaran" name="pengeluaran" method="POST" enctype="multipart/form-data">
                         <input type="hidden" name="id" id="id">
                         <div class="form-group">
-                            <label for="namaTransaksi">Kategori</label>
+                            <label for="namaTransaksi">Tanggal Pengeluaran</label>
                             <input type="date" name="tgl_pengeluaran" id="tgl_pengeluaran" required class="form-control">
                         </div>
                         <div class="form-group">
